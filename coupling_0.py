6a2ccc1049ffffcf64591123dc3d1b7fc27220bd
Sutirthachowdhury <schowdh4@ur.rochester.edu>
2022-03-31 15:12:57 -0400

    first commit

---
+++ coupling_0.py
@@ -0,0 +1,44 @@
+import numpy as np
+from numpy.random import random
+
+#------------ parameters all in au------
+nstep = 50
+pi  = 3.1415926
+lam = 0.65/27.2114
+diab_DB = 0.005/27.2114
+diab_BA = 0.005/27.2114
+beta = 1052.85
+#----------------------------------------
+
+#------ effective DA couplings -----------
+
+vda_eff = np.zeros(nstep)
+
+for g in range(nstep):
+    dg = g*0.018374 + 10**-10
+
+    vda_eff[g] = - (diab_DB*diab_BA)/dg
+
+#------- prefactor for reaction rate ----------
+
+A = np.zeros(nstep)
+
+for g in range(nstep):
+    dg = g*0.018374 + 10**-10
+    A[g] = 2.0*pi*(vda_eff[g])**2*(1.0/np.sqrt(4.0*pi*lam*(1.0/beta)))
+    A[g] = A[g]*41341.37
+
+
+#========== total rate ===============
+
+f = open("rate_vs_delE_0_coup.txt","w+")
+total_rate = np.zeros(nstep)
+
+for g in range(nstep):
+    dg = g*0.018374 + 10**-10
+
+    total_rate[g] = A[g]*np.exp(-(lam)**2/(4.0*lam*(1.0/beta)))
+
+    f.write(f"{dg*27.2114} {total_rate[g]} \n")    
+
+f.close()
